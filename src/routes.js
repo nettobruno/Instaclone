e8b125fdf655efd4d7de186c24da0b486ec660d1
Bruno Netto <brunonettomac@outlook.com>
2020-03-09 09:57:09 -0300

    Add project

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Logo from './assets/instagram.png';
+import Feed from './pages/Feed';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Feed
+  }, {
+    headerTitleAlign: 'center',
+    defaultNavigationOptions: {
+      headerTitle: () => <Image source={Logo} />,
+      headerStyle: {
+        backgroundColor: '#f5f5f5'
+      },
+    },
+  })
+);
+
+export default Routes;
